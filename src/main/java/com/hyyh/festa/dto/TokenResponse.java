c5f3f3b975d4a954ca6cd93f8debaf1e4520fcd4
Jiseok Son <jiseokson@gmail.com>
2024-07-26 17:54:59 +0900

    [Feat] Jwt Filter 구현, Admin login 통합
     Admin login 통해 발급받은 인가 토큰을 이용해 권한 필요한 엔드포인트 접근 제어 구현

--- src/main/java/com/hyyh/festa/dto/TokenResponse.java
+++ src/main/java/com/hyyh/festa/dto/TokenResponse.java
@@ -2,11 +2,24 @@ package com.hyyh.festa.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 @Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class TokenResponse {
-    private String accessToken;
+    private Integer status;
+    private String message;
+    private List<?> data;
+
+    public TokenResponse(String token) {
+        this.status = 200;
+        this.message = "어드민 인가 토큰 발급";
+        this.data = List.of(new inner(token));
+    }
+
+    @Data
+    @AllArgsConstructor
+    public class inner {
+        private String accessToken;
+    }
 }
--- src/main/java/com/hyyh/festa/jwt/JwtAuthenticationFilter.java
+++ src/main/java/com/hyyh/festa/jwt/JwtAuthenticationFilter.java
@@ -1,7 +1,7 @@
 package com.hyyh.festa.jwt;
 
-import com.hyyh.festa.domain.AdminUser;
 import com.hyyh.festa.repository.AdminUserRepository;
+import com.hyyh.festa.repository.FestaUserRepository;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -9,6 +9,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -19,56 +20,38 @@ import java.io.IOException;
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
     private final AdminUserRepository adminUserRepository;
+    private final FestaUserRepository festaUserRepository;
     private final JwtUtil jwtUtil;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        // ‼️ for test
-        // ‼️ for test
-        // ‼️ for test
         final String header = request.getHeader("Authorization");
-        if (header == null) {
+        if (header == null || !header.startsWith("Bearer")) {
             filterChain.doFilter(request, response);
             return;
         }
-        String username = header.split(" ")[1].trim();
-        AdminUser adminUser = adminUserRepository.findByUsername(username).orElse(null);
+        String token = header.substring(7);
+
+        UserDetails userDetails = null;
+        if (jwtUtil.extractUsertype(token).equals("ADMIN")) {
+            userDetails = adminUserRepository
+                    .findByUsername(jwtUtil.extractSubject(token))
+                    .orElse(null);
+        } else {
+            userDetails = festaUserRepository
+                    .findByKakaoSub(jwtUtil.extractSubject(token))
+                    .orElse(null);
+        }
+        if (userDetails == null) {
+            // todo: 인증 실패 응답
+            filterChain.doFilter(request, response);
+            return;
+        }
+
         UsernamePasswordAuthenticationToken authenticationToken =
-                new UsernamePasswordAuthenticationToken(adminUser, null, adminUser.getAuthorities());
+                new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
         authenticationToken.setDetails(new WebAuthenticationDetailsSource());
         SecurityContextHolder.getContext().setAuthentication(authenticationToken);
         filterChain.doFilter(request, response);
-        // ‼️ for test
-        // ‼️ for test
-        // ‼️ for test
-
-//        final String header = request.getHeader("Authorization");
-//        if (header == null || !header.startsWith("Bearer")) {
-//            filterChain.doFilter(request, response);
-//            return;
-//        }
-//        String token = header.substring(7);
-//
-//        UserDetails userDetails = null;
-//        if (jwtUtil.extractAuthority(token) == "ADMIN") {
-//            userDetails = adminUserRepository
-//                    .findByUsername(jwtUtil.extractSubject(token))
-//                    .orElse(null);
-//        } else {
-//            userDetails = temporaryUserRepository
-//                    .findBySubject(jwtUtil.extractSubject(token))
-//                    .orElse(null);
-//        }
-//        if (userDetails == null) {
-//            // todo: 인증 실패 응답
-//            filterChain.doFilter(request, response);
-//            return;
-//        }
-//
-//        UsernamePasswordAuthenticationToken authenticationToken =
-//                new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
-//        authenticationToken.setDetails(new WebAuthenticationDetailsSource());
-//        SecurityContextHolder.getContext().setAuthentication(authenticationToken);
-//        filterChain.doFilter(request, response);
     }
 }
