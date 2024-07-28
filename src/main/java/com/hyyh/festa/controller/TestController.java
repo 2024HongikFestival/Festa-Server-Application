83598b3f6aef31f6c08e74bdc11523fa97f736fc
Jiseok Son <jiseokson@gmail.com>
2024-07-28 17:53:44 +0900

    [Chore] 엔드포인트 권한 설정

--- src/main/java/com/hyyh/festa/controller/TestController.java
+++ src/main/java/com/hyyh/festa/controller/TestController.java
@@ -12,6 +12,20 @@ public class TestController {
     public ResponseEntity<String> test(@AuthenticationPrincipal UserDetails userDetails) {
         return ResponseEntity
                 .status(200)
-                .body("Hello " + userDetails.getUsername());
+                .body("Hello " + userDetails.getUsername() + "! You are successfully authenticated.");
+    }
+
+    @GetMapping("/test/admin")
+    public ResponseEntity<String> testAdmin(@AuthenticationPrincipal UserDetails userDetails) {
+        return ResponseEntity
+                .status(200)
+                .body("Hello admin '" + userDetails.getUsername() + "'. You are authenticated as admin.");
+    }
+
+    @GetMapping("/test/user")
+    public ResponseEntity<String> testUser(@AuthenticationPrincipal UserDetails userDetails) {
+        return ResponseEntity
+                .status(200)
+                .body("Hello user '" + userDetails.getUsername() + "'. You are authenticated as user.");
     }
 }
