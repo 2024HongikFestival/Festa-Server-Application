a9bb8e79dacf2461bdd6bba92adb9dd47a60c431
power-minu <mw0677@naver.com>
2024-07-30 09:16:08 +0900

    [Fix] 응모 POST 요청 양식 수정 1. 와이어프레임에 맞게, 코멘트는 최대 500자까지입니다. 2. 위치 정보는 코드 생성 시 받기 때문에, 추후 다른 DTO에서 받도록 합니다.

--- src/main/java/com/hyyh/festa/dto/EntryPostRequest.java
+++ src/main/java/com/hyyh/festa/dto/EntryPostRequest.java
@@ -22,12 +22,6 @@ public class EntryPostRequest {
     @Pattern(regexp = "^\\d{3}-\\d{4}-\\d{4}$", message = "전화번호의 형식은 000-0000-0000과 같아야 합니다.")
     private String phone;
 
-    @Size(max = 1000, message = "코멘트는 최대 1000글자까지 입력할 수 있습니다.")
+    @Size(max = 500, message = "코멘트는 최대 500글자까지 입력할 수 있습니다.")
     private String comment;
-
-    @NotNull(message = "위치 정보가 필요합니다.")
-    private BigDecimal latitude;
-
-    @NotNull(message = "위치 정보가 필요합니다.")
-    private BigDecimal longtitude;
 }
