a5fafd06e181c33cc7cb7dae71f3648c1cbc728d
hyuk jin Kim <hyukjin.kim.dev@gmail.com>
2024-08-10 19:20:17 +0900

    [Refactor]: findAll 은 default 로 생성되는 함수입니다.

--- src/main/java/com/hyyh/festa/repository/BoothRepository.java
+++ src/main/java/com/hyyh/festa/repository/BoothRepository.java
@@ -8,7 +8,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface BoothRepository extends JpaRepository<Booth, Long> {
 
-    List<Booth> findAll();
-
     List<Booth> findTop3ByOrderByLikeCountDesc();
 }
