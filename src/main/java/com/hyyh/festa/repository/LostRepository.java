ef0ae733af5eb80170e63801c0bc0d03ae299408
Jiseok Son <jiseokson@gmail.com>
2024-08-18 20:19:05 +0900

    [Feat] 사용자 권한에 따라 분실물 게시글 리스트 조회 응답 분기
     - 일반 사용자: lostStatus == PUBLISHED 인 게시글만 조회 가능
     - 어드민 사용자: lostStatus 와 상관없이 조회 가능

--- src/main/java/com/hyyh/festa/repository/LostRepository.java
+++ src/main/java/com/hyyh/festa/repository/LostRepository.java
@@ -2,17 +2,16 @@ package com.hyyh.festa.repository;
 
 import com.hyyh.festa.domain.Lost;
 import com.hyyh.festa.domain.LostStatus;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Repository
 public interface LostRepository extends JpaRepository<Lost, Long> {
-    Page<Lost> findAllByLostStatus(LostStatus lostStatus, Pageable pageable);
-    Page<Lost> findAllByCreatedAtBetweenAndLostStatus(LocalDateTime start, LocalDateTime end, LostStatus lostStatus, Pageable pageable);
-    Page<Lost> findAllByFestaUserKakaoSub(String userId, Pageable pageable);
-    Page<Lost> findAllByCreatedAtBetweenAndFestaUserKakaoSub(LocalDateTime start, LocalDateTime end, String userId, Pageable pageable);
+    List<Lost> findAllByCreatedAtBetween(LocalDateTime start, LocalDateTime end, Pageable pageable);
+    List<Lost> findAllByLostStatus(LostStatus lostStatus, Pageable pageable);
+    List<Lost> findAllByLostStatusAndCreatedAtBetween(LostStatus lostStatus, LocalDateTime start, LocalDateTime end, Pageable pageable);
 }
