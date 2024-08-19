3fa2dd504868907117df9448432ca7c1238c6f2d
다인 <129330821+dainnida@users.noreply.github.com>
2024-08-19 23:20:12 +0900

    [Refactor] 후기 글자 제한수 수정

--- src/main/java/com/hyyh/festa/dto/EntryPostRequest.java
+++ src/main/java/com/hyyh/festa/dto/EntryPostRequest.java
@@ -23,6 +23,6 @@ public class EntryPostRequest {
     private String prize;
 
     @NotBlank(message = "후기를 작성해주세요.")
-    @Size(max = 500, message = "후기는 최대 500글자까지 입력할 수 있습니다.")
+    @Size(max = 100, message = "후기는 최대 100글자까지 입력할 수 있습니다.")
     private String comment;
 }
