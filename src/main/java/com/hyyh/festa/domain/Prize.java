69d1c32ceab73534137e17202ca08e6507a4f649
power-minu <mw0677@naver.com>
2024-08-27 17:09:11 +0900

    [Feat] 수정된 이벤트 api 구현을 위한 엔티티 클래스 수정 1. Prize 클래스에 해당 경품의 수량 필드가 포함됩니다. 2. Entry 클래스에 당첨자 여부 필드가 포함됩니다.

--- src/main/java/com/hyyh/festa/domain/Prize.java
+++ src/main/java/com/hyyh/festa/domain/Prize.java
@@ -1,8 +1,16 @@
 package com.hyyh.festa.domain;
 
 public enum Prize {
-    에어팟,
-    변신로봇,
-    장검,
+    A("에어팟", 5),
+    B("변신로봇", 3),
+    C("장검", 7),
     ;
+
+    public final String name;
+    public final int quantity;
+
+    Prize(String name, int quantity) {
+        this.name = name;
+        this.quantity = quantity;
+    }
 }
